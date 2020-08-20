479f1b4b2deb455b8e4bde153391f72c92e52788
Amir Mostafa Ahmed <53713296+Amir-Mostafa@users.noreply.github.com>
2020-08-21 01:04:53 +0200

    41 file uploaded

---
+++ js/main.js
@@ -0,0 +1,24 @@
+
+function dived()
+{
+    var mail=document.getElementById("mail").value;
+    document.getElementById("name").value = mail.slice(0,mail.indexOf("@"));
+    document.getElementById("domain").value = mail.slice(mail.indexOf("@")+1);
+}
+
+$(document).ready(function(){
+	$(window).scroll(function () {
+			if ($(this).scrollTop() > 50) {
+				$('#back-to-top').fadeIn();
+			} else {
+				$('#back-to-top').fadeOut();
+			}
+		});
+		// scroll body to 0px on click
+		$('#back-to-top').click(function () {
+			$('body,html').animate({
+				scrollTop: 0
+			}, 400);
+			return false;
+		});
+});
